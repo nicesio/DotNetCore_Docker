a0f82e7eb3ce2237cb1b82932d6f80af4099aa23
NOTE1426\nicesio.carvalho <nicesio.carvalho@gsw.com.br>
2022-04-01 19:29:18 -0300

    Introdução ao EFCORE

--- CursoDotNetCore.WebApi/Controllers/AlunoController.cs
+++ CursoDotNetCore.WebApi/Controllers/AlunoController.cs
@@ -1,74 +1,79 @@
 using System.Collections.Generic;
 using System.Linq;
+using CursoDotNetCore.WebApi.Controllers.Data;
 using CursoDotNetCore.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoDotNetCore.WebApi.Controllers
 {
     [ApiController]
     [Route("api/aluno")]
-    public class AlunoController: ControllerBase
+    public class AlunoController : ControllerBase
     {
-        public List<Aluno> Alunos = new List<Aluno>(){
-            new Aluno(){
-                Id = 1,
-                Nome = "João",
-                SobreNome = "Nunes",
-                Telefone = "63254632"
-            }, 
-            new Aluno(){
-                Id = 2,
-                Nome = "Marta",
-                SobreNome = "Nunes",
-                Telefone = "63254632"
-            }, 
-            new Aluno(){
-                Id = 3,
-                Nome = "Nunes",
-                SobreNome = "Nunes",
-                Telefone = "63254632"
-            }, 
-        };
-        public AlunoController()
+        private readonly DataContext _context;
+
+        public AlunoController(DataContext context)
         {
-            
+            _context = context;
+
         }
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(Alunos);
+            return Ok(_context.Alunos);
         }
         [HttpGet("byId/{id}")]
         public IActionResult GetById(int id)
         {
-            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
-            
-            if(aluno == null){
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+
+            if (aluno == null)
+            {
                 return BadRequest("Aluno não Encontrado");
-            }else
+            }
+            else
                 return Ok(aluno);
         }
         [HttpPost]
         public IActionResult Post(Aluno aluno)
         {
+            _context.Add(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
 
         [HttpPut("{id}}")]
         public IActionResult Put(int id, Aluno aluno)
         {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if(alu == null)
+                return BadRequest("Aluno não encontrado");
+            
+            _context.Update(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
 
         [HttpPatch("{id}}")]
         public IActionResult Patch(int id, Aluno aluno)
         {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if(alu == null)
+                return BadRequest("Aluno não encontrado");
+            _context.Update(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
 
         [HttpDelete("{id}}")]
         public IActionResult Delete(int id)
         {
+            var aluno = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if(aluno == null)
+                return BadRequest("Aluno não encontrado");
+            _context.Remove(aluno);
+            _context.SaveChanges();
             return Ok();
         }
     }
--- CursoDotNetCore.WebApi/Controllers/ProfessorController.cs
+++ CursoDotNetCore.WebApi/Controllers/ProfessorController.cs
@@ -1,4 +1,8 @@
+using System.Linq;
+using CursoDotNetCore.WebApi.Controllers.Data;
+using CursoDotNetCore.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoDotNetCore.WebApi.Controllers
 {
@@ -6,10 +10,69 @@ namespace CursoDotNetCore.WebApi.Controllers
     [Route("api/controller")]
     public class ProfessorController: ControllerBase
     {
-        public ProfessorController(){}
+        private readonly DataContext _context;
+
+        public ProfessorController(DataContext context)
+        {
+            _context = context;
+
+        }
         [HttpGet]
         public IActionResult Get()
         {
+            return Ok(_context.Professores);
+        }
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var professor = _context.Professores.FirstOrDefault(a => a.Id == id);
+
+            if (professor == null)
+            {
+                return BadRequest("Professor não Encontrado");
+            }
+            else
+                return Ok(professor);
+        }
+        [HttpPost]
+        public IActionResult Post(Professor professor)
+        {
+            _context.Add(professor);
+            _context.SaveChanges();
+            return Ok(professor);
+        }
+
+        [HttpPut("{id}}")]
+        public IActionResult Put(int id, Professor professor)
+        {
+            var alu = _context.Professores.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if(alu == null)
+                return BadRequest("Professor não encontrado");
+            
+            _context.Update(professor);
+            _context.SaveChanges();
+            return Ok(professor);
+        }
+
+        [HttpPatch("{id}}")]
+        public IActionResult Patch(int id, Professor professor)
+        {
+            var alu = _context.Professores.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if(alu == null)
+                return BadRequest("Professor não encontrado");
+            _context.Update(professor);
+            _context.SaveChanges();
+            return Ok(professor);
+        }
+
+        [HttpDelete("{id}}")]
+        public IActionResult Delete(int id)
+        {
+            var prof = _context.Professores.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if(prof == null)
+                return BadRequest("Professor não encontrado");
+            _context.Remove(prof);
+            _context.SaveChanges();
             return Ok();
         }
     }
